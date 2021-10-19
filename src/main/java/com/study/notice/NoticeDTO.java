90a4107173d7c2de193bc18b25612b9b9e1cec09
yh77yh <yh77yh@gmail.com>
2021-10-19 10:55:17 +0900

    spring boot shopping

---
+++ src/main/java/com/study/notice/NoticeDTO.java
@@ -0,0 +1,15 @@
+package com.study.notice;
+
+public class NoticeDTO {
+	@Data
+	public class NoticeDTO {
+	  private int    noticeno     ;
+	  private String title        ;
+	  private String content      ;
+	  private String wname        ;
+	  private String passwd       ;
+	  private int    cnt          ;
+	  private String rdate        ;
+	 
+	}
+}
